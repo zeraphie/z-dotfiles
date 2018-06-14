4d73c68b2b5d30adf3bd9cb45c61f4db9690b760
Izzy Skye <izzy.skye@mindoktor.se>
2018-06-14 18:20:13 +0200

    Change the way scripts are loaded

--- .bash/work/load.sh
+++ .bash/work/load.sh
@@ -2,5 +2,8 @@
 # Load work specifics                                                          #
 ################################################################################
 for file in ~/.bash/work/*/*; do
-    source "$file"
+    # Don't include this file and get only bash files
+    if [[ $file != *"load"* ]] && [[ $file == *.sh ]]; then
+        source "$file"
+    fi
 done
